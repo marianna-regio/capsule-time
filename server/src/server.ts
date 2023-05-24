5a30a4831a96cfb1357ee2979acd63432c647c97
Marianna Regio <mariannaregio@gmail.com>
2023-05-24 01:46:57 -0300

    first commit project web/server

---
+++ server/src/server.ts
@@ -0,0 +1,43 @@
+// QUANDO O SERVIDOR FALHAR EXECUTAR O ARQUIVO COM npm run dev//
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
+
+
+const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname,'../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(cors, {
+  origin: true, 
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(memoriesRoutes)
+app.register(authRoutes)
+app.register(uploadRoutes)
+
+// ouvindo a porta 3333//
+app
+  .listen({
+    port: 3333,
+    //host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
